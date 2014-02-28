933778442cddd85f5df66f6128403016a008273d
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-02-28 15:37:16 -0500

    Add cling for static assets

--- wsgi.py
+++ wsgi.py
@@ -21,12 +21,8 @@ sys.path.append(os.path.join(CURR_DIR, 'src'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+from dj_static import Cling
+application = Cling(application)
